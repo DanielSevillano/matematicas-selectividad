9539c4adc57224ecf56b1f14205818045a5193b2
Daniel <daniel-sevillano@hotmail.com>
2023-08-02 22:54:41 +0200

    Primera versión.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Matemáticas de Selectividad
+
+Una página web para ver los exámenes de Selectividad de matemáticas de Andalucía.
+
+## 📅 Hoja de ruta
+
+- [x] Visualización de exámenes
+- [x] Impresión de exámenes
+- [ ] Navegación de exámenes por año
+- [ ] Categorías de ejercicios
+- [ ] Página de inicio con subpáginas de exámenes y ejercicios
+- [ ] Navegación de ejercicios por categoría
+- [ ] Funcionalidad para generar un examen aleatorio
+
+## 📖 Contenido
+
+- [x] 2023
+    - [x] Junio
+- [x] 2022
+- [ ] 2021
+    - [x] Junio
+    - [x] Septiembre
+- [ ] 2020
+    - [x] Junio
+    - [x] Septiembre
---
+++ main.js
@@ -0,0 +1,102 @@
+window.MathJax = {
+    tex: {
+        inlineMath: [["$", "$"]],
+    },
+};
+
+async function mostrarNavegacion() {
+    const navegacion = document.querySelector("nav");
+
+    const respuesta = await fetch("data\\index.json");
+    const datos = await respuesta.json();
+
+    datos.forEach(examen => {
+        examen.ediciones.forEach(edicion => {
+            let texto = "";
+            if (edicion == 0) texto += "Junio de ";
+            else if (edicion == 5) texto += "Septiembre de ";
+            else texto += "Reserva " + edicion + " de ";
+            texto += examen.curso
+            console.log(texto);
+
+            const boton = document.createElement("button");
+            boton.textContent = texto;
+            boton.addEventListener("click", function () {
+                mostrarExamen(examen.curso * 10 + edicion);
+            })
+            navegacion.append(boton);
+        })
+    });
+}
+
+async function obtenerEjercicio(examen, ejercicio) {
+    const parrafo = document.createElement("p");
+
+    const curso = String(examen).slice(0, 4);
+    const ruta = "data\\" + curso + "\\" + examen + ejercicio + ".txt";
+
+    const respuesta = await fetch(ruta);
+    const datos = await respuesta.text();
+
+    parrafo.innerHTML = datos;
+    return parrafo
+}
+
+async function obtenerExamen(examen) {
+    const articulo = document.createElement("article");
+
+    const titulo = document.createElement("h2");
+    const codigo = String(examen);
+    const curso = codigo.slice(0, 4);
+    const edicion = codigo.slice(-1)
+
+    let texto = "";
+    if (edicion == 0) texto += "Junio de ";
+    else if (edicion == 5) texto += "Septiembre de ";
+    else texto += "Reserva " + edicion + " de ";
+    texto += curso;
+    titulo.innerText = texto;
+
+    const boton = document.createElement("button");
+    boton.textContent = "🖨️ Imprimir";
+    boton.addEventListener("click", function () {
+        window.print()
+    });
+    titulo.append(boton);
+    articulo.append(titulo);
+
+    const lista = document.createElement("ol");
+    for (let i = 1; i <= 8; i++) {
+        const elemento = document.createElement("li");
+        const parrafo = await obtenerEjercicio(examen, i);
+        elemento.append(parrafo);
+        lista.append(elemento);
+    }
+    articulo.append(lista);
+
+    return articulo
+}
+
+async function mostrarEjercicio(examen, ejercicio) {
+    const main = document.querySelector("main");
+
+    const parrafo = await obtenerEjercicio(examen, ejercicio);
+    main.append(parrafo);
+    MathJax.typeset();
+}
+
+async function mostrarExamen(examen) {
+    const main = document.querySelector("main");
+
+    const articulos = document.getElementsByTagName("article");
+    for (const elemento of articulos) {
+        elemento.remove();
+    }
+
+    const articulo = await obtenerExamen(examen);
+    main.append(articulo);
+    MathJax.typeset();
+}
+
+mostrarNavegacion();
+mostrarExamen(20230);
