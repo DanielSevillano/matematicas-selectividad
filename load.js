f48b049a18de4381271c3e9703fee6ec33066fde
Daniel <daniel-sevillano@hotmail.com>
2023-08-07 16:00:12 +0200

    Reordenando la carga de JS.

---
+++ load.js
@@ -0,0 +1,12 @@
+const scriptMathJax = document.createElement("script");
+scriptMathJax.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+
+const scriptMain = document.createElement("script");
+scriptMain.src = "main.js";
+scriptMain.async = false;
+
+scriptMain.addEventListener("load", function () {
+    document.head.append(scriptMathJax);
+});
+
+document.head.append(scriptMain);
