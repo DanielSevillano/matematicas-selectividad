f98c59910208204dfed96a9e6650344d14752c9f
Daniel <daniel-sevillano@hotmail.com>
2023-08-13 15:16:10 +0200

    Implementación de la navegación por categorías.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.MathJax = {
     },
 };
 
-async function mostrarNavegacion() {
+async function mostrarNavegacionExamenes() {
     const navegacion = document.querySelector("nav");
     const navegacionExamenes = document.createElement("fluent-tabs");
 
@@ -53,6 +53,48 @@ async function mostrarNavegacion() {
     })
 
     navegacion.append(navegacionExamenes);
+    navegacion.querySelector("fluent-button").click();
+}
+
+async function mostrarNavegacionEjercicios() {
+    const navegacion = document.querySelector("nav");
+    const navegacionEjercicios = document.createElement("fluent-tabs");
+
+    const respuesta = await fetch("data\\tags.json");
+    const datos = await respuesta.json();
+
+    datos.forEach(bloque => {
+        const seccionBloque = document.createElement("fluent-tab");
+        seccionBloque.textContent = bloque.bloque;
+        navegacionEjercicios.append(seccionBloque);
+
+        const navegacionCategorias = document.createElement("fluent-tab-panel");
+        const contenedor = document.createElement("div");
+
+        bloque.categorias.forEach(categoria => {
+            const botonCategoria = document.createElement("fluent-button");
+            botonCategoria.textContent = categoria;
+
+            botonCategoria.addEventListener("click", function () {
+                if (this.appearance == "neutral") {
+                    mostrarCategoria(categoria);
+
+                    const botones = navegacion.getElementsByTagName("fluent-button");
+                    for (let boton of botones) {
+                        if (boton == this) boton.appearance = "accent";
+                        else boton.appearance = "neutral";
+                    }
+                }
+            });
+
+            contenedor.append(botonCategoria);
+            navegacionCategorias.append(contenedor);
+        });
+
+        navegacionEjercicios.append(navegacionCategorias);
+    });
+
+    navegacion.append(navegacionEjercicios);
     navegacion.querySelector("fluent-button").click();
 }
 
@@ -170,12 +212,20 @@ async function obtenerExamen(examen) {
     return articulo
 }
 
-async function mostrarEjercicio(examen, ejercicio) {
-    const main = document.querySelector("main");
+async function obtenerCategoria(categoria) {
+    const articulo = document.createElement("article");
 
-    const parrafo = await obtenerEjercicio(examen, ejercicio);
-    main.append(parrafo);
-    MathJax.typeset();
+    const respuesta = await fetch("data\\metadata.json");
+    const datos = await respuesta.json();
+
+    const ejercicios = datos.filter(ejercicio => ejercicio.categorias.includes(categoria));
+
+    for (let ejercicio of ejercicios) {
+        const parrafo = await obtenerEjercicio(parseInt(ejercicio.ejercicio / 10), ejercicio.ejercicio % 10);
+        articulo.append(parrafo);
+    };
+
+    return articulo
 }
 
 async function mostrarExamen(examen) {
@@ -190,4 +240,16 @@ async function mostrarExamen(examen) {
     MathJax.typeset();
 }
 
-mostrarNavegacion();
+async function mostrarCategoria(categoria) {
+    const main = document.querySelector("main");
+    const carga = document.createElement("fluent-progress-ring");
+    main.textContent = "";
+    main.append(carga);
+
+    const articulo = await obtenerCategoria(categoria);
+    main.textContent = "";
+    main.append(articulo);
+    MathJax.typeset();
+}
+
+mostrarNavegacionExamenes();
