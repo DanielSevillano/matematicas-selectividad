52a0e4d993e565a87f0af81b3dc1c6b4d900d50b
Daniel <daniel-sevillano@hotmail.com>
2023-10-24 16:42:32 +0200

    Mejorando el examen aleatorio.

--- js/examenes.js
+++ js/examenes.js
@@ -48,7 +48,11 @@ botones.forEach(boton => {
 });
 
 botonAleatorio.addEventListener("click", () => {
-    botones[Math.floor(Math.random() * botones.length)].click();
+    const boton = botones[Math.floor(Math.random() * botones.length)];
+    const curso = boton.id.replace("boton-", "").slice(0, 4);
+    boton.click();
+    console.log(curso);
+    document.querySelector("#grupo-" + curso).click();
 });
 
 if (!examen) document.querySelector(".grupo").click();
