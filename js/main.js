9d7759574e734ae686dc22aad9b64160128dba49
Daniel <daniel-sevillano@hotmail.com>
2024-01-27 13:41:36 +0100

    Añadiendo el ejercicio diario de sociales.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 import { formatear, obtenerEjercicio } from "./math.js";
 
-async function ejercicioDiario() {
-    const contenedor = document.querySelector("#ejercicio-diario");
+async function ejercicioDiario(seccion) {
+    const contenedor = document.querySelector("#ejercicios-diarios");
+
+    const titulo = document.createElement("h3");
+    titulo.textContent = seccion;
+
+    const modalidad = seccion.toLowerCase();
 
     const fecha = new Date();
     const inicio = new Date(fecha.getFullYear(), 0, 1);
@@ -9,17 +14,22 @@ async function ejercicioDiario() {
     const semilla = Math.ceil(diferencia / 86400000);
     const indice = (semilla * 115) % 366;
 
-    const respuesta = await fetch("data\\ciencias\\metadata.json");
+    const respuesta = await fetch("data\\" + modalidad + "\\metadata.json");
     const datos = await respuesta.json();
 
     const objeto = datos[indice];
     const codigo = String(objeto.ejercicio);
-    const articulo = await obtenerEjercicio("ciencias", codigo.slice(0, 5), codigo.slice(-1), false, objeto.categorias, true);
+    const articulo = await obtenerEjercicio(modalidad, codigo.slice(0, 5), codigo.slice(-1), false, objeto.categorias, true);
     articulo.classList.add("tarjeta");
 
+    articulo.prepend(titulo);
     contenedor.append(articulo);
     formatear(articulo);
     contenedor.classList.remove("cargando");
 }
 
-ejercicioDiario();
+async function ejerciciosDiarios() {
+    ejercicioDiario("Ciencias").then(() => ejercicioDiario("Sociales"));
+}
+
+ejerciciosDiarios();
